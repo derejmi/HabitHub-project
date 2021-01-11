51eb254bf293bacfad174377d56990df7270c460
derejmi <derejemireyigbe@gmail.com>
2021-01-11 14:49:37 +0000

    add find by email method to user model

--- Server/models/users.js
+++ Server/models/users.js
@@ -33,6 +33,19 @@ class User {
       }
     });
   }
+
+  static findByEmail(email) {
+    return new Promise(async (res, rej) => {
+      try {
+        let result = await db.run(SQL`SELECT * FROM users
+                                                WHERE email = ${email};`);
+        let user = new User(result.rows[0]);
+        res(user);
+      } catch (err) {
+        rej(`Error retrieving user: ${err}`);
+      }
+    });
+  }
 }
 
 module.exports = User;
