c1a6036c7e8d5e329477dd6f9afd91bf23ba1492
derejmi <derejemireyigbe@gmail.com>
2021-01-12 12:23:00 +0000

    finish passport config

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -1,21 +1,21 @@
 //We use this to tell passport we want to use the strategy for handling JSON web tokens
-const JwtStrategy = require("passport-jwt").Strategy;
+var JwtStrategy = require("passport-jwt").Strategy;
 
 //we then grab the ExtractJwt method
-const ExtractJwt = require("passport-jwt").ExtractJwt;
+var ExtractJwt = require("passport-jwt").ExtractJwt;
 
 //We grab our user model
-const User = "../models/users.js";
+const User = require("../models/users.js");
 
 //We grab our keys
-const keys = require("./keys");
+var keys = require("./keys");
 
 //empty options hash
-const options = {};
+var options = {};
 
 //where to extract the request from - built into passport-jwt look at passport-jwt docs
 //this will extract the bearer token/JWT from the header
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("Bearer");
 
 //we want our secret or key so we know what to check against when checking to see if things are signed
 options.secretOrKey = keys.secretOrKey;
@@ -25,9 +25,20 @@ options.secretOrKey = keys.secretOrKey;
 //the jwt strategt takes in the options hash and a callback (our call back takes the jwt_payload which)
 //we have specified
 module.exports = (passport) => {
+  console.log("hi");
+  console.log(options, "options");
   passport.use(
-    new JwtStrategy(options, (jwt_payload, done) => {
-      console.log(jwt_payload);
+    new JwtStrategy(options, function (jwt_payload, done) {
+      console.log(jwt_payload, "jwt");
+      User.findByEmail(jwt_payload.email)
+        .then((userData) => {
+          const user = userData.rows[0];
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch((err) => console.warn(err));
     })
   );
 };
