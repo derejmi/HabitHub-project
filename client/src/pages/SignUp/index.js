1b0670f05c36f6a13e99ca7c06bfe71a509bb4aa
PaolaDM <orchidea.preziosa@gmail.com>
2021-01-13 02:37:51 +0000

    set signUp function

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -5,10 +5,49 @@ import Form from 'react-bootstrap/Form';
 
 
 class SignUp extends Component {
-
   state = {
-
+      username: "",
+      email: "",
+      password: "",
+      confPassword: ""
   };
+
+  handleInput = e => this.setState({ [e.target.name]: e.target.value})
+  // formIncomplete = () => Object.values(this.state).some(v => !v) || this.state.password !== this.state.passwordConfirmation
+
+
+  signup = async (e) => {
+    e.preventDefault();
+    try {
+        const userData = { 
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+        const options = {
+            method: 'POST',
+            //check header settings with Daré
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userData)
+        }
+        //check server route
+        const r = await fetch(`http://localhost:3000/signup/registration`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err) }
+        this.props.login(userData);
+        //this.props.history.push('./feed')
+    } catch (err) {
+        console.warn(err);
+        this.setState({
+            username: "",
+            email: "",
+            password: "",
+            confPassword: ""
+        })
+    }
+  };
+
+
 
   render(){
     return(
@@ -16,14 +55,18 @@ class SignUp extends Component {
         <h1 className="titleHabitHub">Welcome to HabitHub</h1>
         
         <p> Create a new user:</p>
-        <Form action="">
+        <Form onSubmit={this.signup}>
             <label for="username">Username:</label>
-            <input type="text" name="credentials" id="username"/><br/>
+            <input id="username" type="text" name="username" value={this.state.username}  onChange={this.handleInput} placeholder="username" /><br/>
             <label for="password">Password:</label>
-            <input type="text" name="credentials" id="password"/><br/>
+            <input id="password"  type="text" name="password" value={this.state.password} onChange={this.handleInput} placeholder="password" /><br/>
+            <label for="password">Confirm password:</label>
+            <input id="confPassword"  type="text" name="confPassword" value={this.state.confPassword} onChange={this.handleInput} placeholder="Confirm password" /><br/>
             <label for="email">Email:</label>
-            <input type="email" name="credentials" id="email"/><br/>
-            <Button variant="primary" type="submit">Submit details</Button>            
+            <input id="email" type="email" name="email" value={this.state.email} onChange={this.handleInput} placeholder="email" /><br/>
+            
+            {/* <Button variant="primary" type="submit" className={this.formIncomplete() ? 'disabled' : 'enabled'} disabled={this.formIncomplete()} >Submit details</Button>             */}
+          <Button className="genButtons" variant="info" type="submit" >Submit details</Button> 
         </Form>
       </Jumbotron>
     );
