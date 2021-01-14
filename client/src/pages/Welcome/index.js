4972b066eb6134d82e565ac89f8fa5a9b7e70972
PaolaDM <orchidea.preziosa@gmail.com>
2021-01-14 21:16:15 +0000

    fixed activeLink bug &  add motivational quote

--- client/src/pages/Welcome/index.js
+++ client/src/pages/Welcome/index.js
@@ -9,13 +9,13 @@ const Welcome = () => {
         <>
             <Jumbotron className="jumbotron">
                 <nav className="navbar">
-                    <Link to="/login"  className="buttons" activeClassName="current">Log In</Link>
-                    <Link to="/signup" className="buttons" activeClassName="current">Sign Up</Link>            
+                    <Link to="/login"  className="buttons" >Log In</Link>
+                    <Link to="/signup" className="buttons" >Sign Up</Link>            
                 </nav>
                 <video className="welcomeVideo" autostart="true" autoPlay src={backgroundVideo} type="video/mp4" />
             </Jumbotron>
             <div className="quotationContainer">
-                <h2 className="motivationalQuote">motivational quote here </h2>
+                <h2 className="motivationalQuote">When someone says you can't do it ... <br/> do it twice and take pictures </h2>
             </div>
             
         </>
